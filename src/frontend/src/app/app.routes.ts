ef57d97ce5f9a49b2a356c58feef2c44223eef3f
Bureeeek <burakkocbk653@gmail.com>
2024-11-19 09:22:18 +0100

    Navbar
    Navbar funktioniert nicht ganz aber man kann in die route. Werde ich noch zu Hause weitermachen

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AccountComponent } from './account/account.component';
+import { CreateComponent } from './create/create.component';
+import { NewsComponent } from './news/news.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'news', component: NewsComponent },
+  { path: '', redirectTo: '/about', pathMatch: 'full' }
+];
